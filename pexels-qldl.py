56dea178784c79e532e0f1377146314a68dd3d8f
abhira0 <abhishekr0@protonmail.com>
2021-04-30 18:49:47 +0530

    added req.txt

--- pexels-qldl.py
+++ pexels-qldl.py
@@ -1,12 +1,17 @@
+import os
+import sys
+
 import click
-import os,sys
+
 from WS_Pexels import *
 
+
 @click.command()
-@click.option('--query', prompt='Search what? ',
-              help='Query of the request')
+@click.option("--query", prompt="Search what? ", help="Query of the request")
 def download(query):
     PS = pexelScraper()
     PS.queryRequest(query)
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     download()
